a408f0279790144dc286d0c72e9bbe3d842fc859
Mor-Aviv <avivmor002@gmail.com>
2025-04-01 10:27:32 +0300

    added comments

--- test/utils/resetDatabase.ts
+++ test/utils/resetDatabase.ts
@@ -3,6 +3,7 @@ import { Movie } from '../../src/movies/entities/movie.entity';
 import { Showtime } from '../../src/showtimes/entities/showtime.entity';
 import { Booking } from '../../src/bookings/entities/booking.entity';
 
+// helper function to reset data between E2E tests
 export async function resetDatabase(dataSource: DataSource) {
   await dataSource.getRepository(Booking).delete({});
   await dataSource.getRepository(Showtime).delete({});
